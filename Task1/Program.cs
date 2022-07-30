1276826c7cdf279102d0caba10137dc4d0fdbae4
alexey02rus <101921891+alexey02rus@users.noreply.github.com>
2022-07-30 20:58:33 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static double GetAreaTriangle(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2.0;
+            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return S;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон первого треугольника.");
+            Console.Write("a1 = ");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b1 = ");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c1 = ");
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите длины сторон второго треугольника.");
+            Console.Write("a2 = ");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b2 = ");
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c2 = ");
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            double s1 = GetAreaTriangle(a1, b1, c1);
+            double s2 = GetAreaTriangle(a2, b2, c2);
+            if (s1 > s2)
+            {
+                Console.WriteLine($"Площадь первого треугольника больше ({s1:.00} > {s2:.00}).");
+            }
+            else if (s1 < s2)
+            {
+                Console.WriteLine($"Площадь второго треугольника больше ({s1:.00} < {s2:.00}).");
+            }
+            else
+            {
+                Console.WriteLine($"Площади треугольников равны ({s1:.00}).");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void GetBoxAreaVolume(double a, out double s, out double v)
+        {
+            v = Math.Pow(a, 3);
+            s = 6 * (a * a);
+            Console.WriteLine("Площадь поверхности куба: {0:.00}", s);
+            Console.WriteLine("Объем куба: {0:.00}", v);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            Console.Write("A = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetBoxAreaVolume(a, out s, out v);
+            Console.ReadKey();
+        }
+    }
+}
